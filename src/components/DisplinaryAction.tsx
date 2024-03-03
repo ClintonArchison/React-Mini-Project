b2f675e6c22629debcc7ae90598e2165edcd271b
ClintonArchison <clintonarchison2109@gmail.com>
2024-03-03 13:24:15 +0530

    commit

--- src/components/DisplinaryAction.tsx
+++ src/components/DisplinaryAction.tsx
@@ -1,43 +1,48 @@
-import { useEffect } from "react";
-
 export function DisplinaryAction({ data, role }: any) {
-  useEffect(() => {
-    // console.log("DISPLINARY ACTION PAGE ONLY ALLOWED FOR ADMIN");
-    if (role !== "ADMIN") {
-      window.location.href = "/unauthorized";
-    }
-  }, []);
-  return (
-    <>
-      <div className="container">
-        <table width="100%" className="table">
-          <thead>
-            <tr>
-              <td colSpan={3} align="center" className="table-primary">
-                <b>DISIPLINARY ACTION</b>
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <b>Disiplinary Action</b>
-              </td>
-              <td>
-                <b>Done On</b>
-              </td>
-              <td align="center">
-                <b>Punishment Given</b>
-              </td>
-            </tr>
-            <tr>
-              <td>{data.students[0].disciplinary_actions}</td>
-              <td>{data.students[0].disciplinary_actions_date}</td>
-              <td align="center">{data.students[0].punishment_given}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+  // console.log("DISPLINARY ACTION PAGE ONLY ALLOWED FOR ADMIN");
+  if (role !== "ADMIN") {
+    return (
+      <p className="text-center h-full">
+        <h3 className="display-3 text-center text-danger">401-Unauthorized</h3>
+        The Page you were accessing earlier is limited to admin users.
+        <br />
+        Please login with admin user credentials <a href="/">Here</a>
+      </p>
+    );
+  }
+  if (role === "ADMIN") {
+    return (
+      <>
+        <div className="container">
+          <table width="100%" className="table">
+            <thead>
+              <tr>
+                <td colSpan={3} align="center" className="table-primary">
+                  <b>DISIPLINARY ACTION</b>
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <b>Disiplinary Action</b>
+                </td>
+                <td>
+                  <b>Done On</b>
+                </td>
+                <td align="center">
+                  <b>Punishment Given</b>
+                </td>
+              </tr>
+              <tr>
+                <td>{data.students[0].disciplinary_actions}</td>
+                <td>{data.students[0].disciplinary_actions_date}</td>
+                <td align="center">{data.students[0].punishment_given}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 }
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -7,29 +7,32 @@ export function SideNav() {
       <ul className="list-group list-group-flush d-flex flex-column ">
         <li className="list-group-item">
           <NavLink
+            type="button" className="list-group-item list-group-item-action"
             to="/dashboard/student-info"
-            className="text-decoration-none"
           >
             Student Info
           </NavLink>
         </li>
         <li className="list-group-item">
           <NavLink
+            type="button" className="list-group-item list-group-item-action"
             to="/dashboard/student-attendance"
-            className="text-decoration-none"
           >
             Student Attendance
           </NavLink>
         </li>
         <li className="list-group-item">
-          <NavLink to="/dashboard/marks" className="text-decoration-none">
+          <NavLink
+            type="button" className="list-group-item list-group-item-action"
+            to="/dashboard/marks"
+          >
             Marks
           </NavLink>
         </li>
         <li className="list-group-item">
           <NavLink
+            type="button" className="list-group-item list-group-item-action"
             to="/dashboard/disciplinary-action"
-            className="text-decoration-none"
           >
             Displinary Action
           </NavLink>
